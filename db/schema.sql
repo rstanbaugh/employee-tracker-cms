6821c81ef61fa080ff4f11f4c3c19c124205e5eb
rstanbaugh <rich_bootcamp@icloud.com>
2022-06-19 14:00:33 -0400

    redo schema to match challenge

--- db/schema.sql
+++ db/schema.sql
@@ -11,19 +11,19 @@ CREATE TABLE department (
 
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  job_title VARCHAR(50) NOT NULL,
-  salary DECIMAL(10,2) NOT NULL
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL NOT NULL,
+  dept_id INTEGER,
+  CONSTRAINT fk_departments FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  dept_id INTEGER NOT NULL,
-  role_id INTEGER NOT NULL,
+  role_id INTEGER,
   manager_id INTEGER,
-  CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  CONSTRAINT fk_departments FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE CASCADE,
+  CONSTRAINT fk_roles FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
   CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -10,36 +10,42 @@ VALUES
   (4, 'Sales'),
   (5, 'IT');
 
-INSERT INTO role (id, job_title, salary)
+INSERT INTO role (id, title, salary, dept_id)
 VALUES
-  (1, 'Manager', 100000),
-  (2, 'Engineer', 150000),
-  (3, 'Accountant', 50000),
-  (4, 'Associate', 50000);
+  (1, 'Manager', '250000', 1),
+  (2, 'Manager', '100000', 2),
+  (3, 'Manager', '125000', 3),
+  (4, 'Manager', '200000', 4),
+  (5, 'Manager', '200000', 5),
+  (6, 'Accountant', '50000', 2),
+  (7, 'Associate', '100000', 3),
+  (8, 'Associate', '150000', 4),
+  (9, 'Engineer', '150000', 4),
+  (10, 'Engineer', '150000', 5);
 
-INSERT INTO employee (id, first_name, last_name, dept_id, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
-  (1, 'James', 'Fraser', 1, 1, NULL),
-  (2, 'Jack', 'London', 2, 1, 1),
-  (3, 'Robert', 'Bruce', 3, 1, 1),
-  (4, 'Peter', 'Greenaway', 4, 1, 1),
-  (5, 'Derek', 'Jarman', 5, 1, 1),
-  (6, 'Paolo', 'Pasolini', 2, 3, 2),
-  (7, 'Heathcote', 'Williams', 2, 3, 2),
-  (8, 'Sandy', 'Powell', 3, 2, 3),
-  (9, 'Emil', 'Zola', 3, 4, 3),
-  (10, 'Sissy', 'Coalpits', 3, 4, 3),
-  (11, 'Antoinette', 'Capet',3, 4, 3),
-  (12, 'Samuel', 'Delany', 3, 4, 3),
-  (13, 'Tony', 'Duvert', 4, 3, 4),
-  (14, 'Dennis', 'Cooper', 4, 4, 4),
-  (15, 'Monica', 'Bellucci',  4, 4, 4),
-  (16, 'Samuel', 'Johnson',  4, 4, 4),
-  (17, 'John', 'Dryden', 4, 4, 4),
-  (18, 'Alexander', 'Pope',  4, 4, 4),
-  (19, 'Lionel', 'Johnson',  4, 4, 4),
-  (20, 'Aubrey', 'Beardsley',  5, 2, 5),
-  (21,'Tulse', 'Luper', 5, 2, 5),
-  (22, 'William', 'Morris',  5, 2, 5),
-  (23, 'George', 'Shaw',  5, 2, 5),
-  (24, 'Arnold', 'Bennett',  5, 2, 5);
+  (1, 'James', 'Fraser', 1, NULL),
+  (2, 'Jack', 'London', 1, 1),
+  (3, 'Robert', 'Bruce', 1, 1),
+  (4, 'Peter', 'Greenway', 1, 1),
+  (5, 'Derek', 'Jarman', 1, 1),
+  (6, 'Paolo', 'Pasolini', 3, 2),
+  (7, 'heathcote', 'Williams', 3, 2),
+  (8, 'Dsandy', 'Powell', 2, 3),
+  (9, 'Emil', 'Zola', 4, 3),
+  (10, 'Sissy', 'Coalpitts', 4, 3),
+  (11, 'Antionette', 'Capet', 4, 3),
+  (12, 'Samuel', 'Delany', 4, 3),
+  (13, 'Tony', 'Duvert', 4, 4),
+  (14, 'Dennis', 'Cooper', 4, 4),
+  (15, 'Monica', 'Bellucci', 4, 4),
+  (16, 'Samuel', 'Johnson', 4, 4),
+  (17, 'John', 'Dryden', 4, 4),
+  (18, 'Alexander', 'Pope', 4, 4),
+  (19, 'Leionel', 'Johnson', 4, 4),
+  (20, 'Aubrey', 'Beardsley', 2, 5),
+  (21, 'Tulse', 'Luper', 2, 5),
+  (22, 'William', 'Morris', 2, 5),
+  (23, 'George', 'Shaw', 2, 5),
+  (24, 'Arnold', 'Bennett', 2, 5);
